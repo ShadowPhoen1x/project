f96ebfdba55c0be6463a1909674b1d2eb6a23c43
Taine <taine.meyer@hotmail.com>
2023-04-18 15:56:00 +1000

    Inital commit, made 3 files and linked them together

---
+++ index.js
@@ -0,0 +1,11 @@
+// API Key: 6053f1a230348aba0c3dd2138e005c79
+// Documentation: https://developers.themoviedb.org/3/getting-started/introduction
+// Support forum: https://www.themoviedb.org/talk/category/5047958519c29526b50017d6
+// Wrappers & libraries: https://www.themoviedb.org/documentation/api/wrappers-libraries
+// Service status: https://status.themoviedb.org
+
+//idea 1.netflix style scroll through movies, click on movie to show the description + a like / dislike button. 3 event listeners there. 
+//idea 2. have a dropdown filter at top of the page, select a genre and have cards pop up with movies. Click on cards to display the movie description. review box at the bottom of each card?
+
+const tmdbKey = "6053f1a230348aba0c3dd2138e005c79";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
