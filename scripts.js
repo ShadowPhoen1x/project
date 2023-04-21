1d99ac883c44489040e8be43759ca8da77fae389
Taine <taine.meyer@hotmail.com>
2023-04-21 16:43:19 +1000

    filter is now working

--- scripts.js
+++ scripts.js
@@ -10,8 +10,40 @@
 // https://www.youtube.com/watch?v=AVmGmLFcukM - another dropdown menu but with API implementation
 
 //idea 1.netflix style scroll through movies, click on movie to show the description + a like / dislike button. 3 event listeners there. 
-//idea 2. have a dropdown filter at top of the page, select a genre and have cards pop up with movies. Click on cards to display the movie description. review box at the bottom of each card?
-//--> HTML logic - start with header, search??, filter then movie cards below. 
+//idea 2. have a dropdown filter at top of the page, select a genre and have cards pop up with movies. Click on cards to display the movie description. review / comment box at the bottom of each card?
+// EL 1. like / dislike the movie? or a fav option
+// EL 2. comment box/ review box
+// EL 3. mouse hover / click, so when you hover or click the movie it displays the description of it? or a pop up card on click. 
 
 const tmdbKey = "6053f1a230348aba0c3dd2138e005c79";
 const tmdbBaseUrl = "https://api.themoviedb.org/3";
+
+const start = async () => {
+    const movies = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=6053f1a230348aba0c3dd2138e005c79`)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+}
+start()
+// fetch genres from the API and make the dropdown menu
+const getGenres = async () => {
+    const genreRequestEndpoint = "/genre/movie/list";
+    const requestParams = `?api_key=${tmdbKey}`;
+    const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+    try {
+      const response = await fetch(urlToFetch);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const genres = jsonResponse.genres;
+        console.log(jsonResponse.genres)
+
+        document.getElementById("genres").innerHTML = `
+        ${genres.map(function (genre) {
+            return `<option>${genre.name}</option>`}).join('')}`
+        return genres;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+getGenres();
